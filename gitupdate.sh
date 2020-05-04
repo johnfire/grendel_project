cb42daf8d22a92c3c2c031dbb5b3092e8ab90718
Christopher Rehm <christopherrehm@web.de>
2020-05-04 22:49:13 +0200

    first commit master grendel repo

---
+++ gitupdate.sh
@@ -0,0 +1,10 @@
+cd grendelShares
+git pull origin master
+cd ../grendelSmallPrograms
+git pull origin master
+cd ../grendelPhysicalOperationProgram
+git submodule update --remote
+git pull origin master
+cd ../grendelAIandDecisionProgram
+git submodule update --remote
+git pull origin master
