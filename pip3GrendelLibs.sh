7207b533edf18f28a4973ceb5a9de01c3ae72283
Christopher Rehm <christopherrehm@web.de>
2020-06-16 17:32:08 +0200

    first commit

---
+++ pip3GrendelLibs.sh
@@ -0,0 +1,7 @@
+pip3 install numpy
+pip3 install pandas
+pip3 install opencv-contrib-python
+pip3 install matplotlib
+pip3 install nltk
+pip3 install scipy ipython jupyter sympy nose tensorflow keras scikit-learn
+pip3 install pytorch torchvision cudatoolkit=10.2 
